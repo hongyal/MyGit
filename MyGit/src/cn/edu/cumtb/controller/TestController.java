7348dd4d03d3214c73626ed2636adbaa8518e1f9
liuhongya <hongyal@163.com>
2016-12-01 21:57:13 +0800

    second commit

--- MyGit/src/cn/edu/cumtb/controller/TestController.java
+++ MyGit/src/cn/edu/cumtb/controller/TestController.java
@@ -4,6 +4,8 @@ public class TestController {
 
 	public static void main(String[] args) {
 		System.out.println("dddd");
+		System.out.println("second submit");
+		System.out.println("aaaaaaaaaaaaa");
 	}
 
 }
