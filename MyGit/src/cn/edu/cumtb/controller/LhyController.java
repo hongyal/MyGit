3797b93675582cc07c4d6ba845a4ebfd6edba63a
liuhongya <hongyal@163.com>
2016-12-01 22:29:28 +0800

    000000000

--- MyGit/src/cn/edu/cumtb/controller/LhyController.java
+++ MyGit/src/cn/edu/cumtb/controller/LhyController.java
@@ -5,6 +5,7 @@ public class LhyController {
 	public static void main(String[] args) {
 		System.out.println("my own branch");
 		System.out.println("dddd");
+		System.out.println("000000000000000000");
 	}
 
 }
